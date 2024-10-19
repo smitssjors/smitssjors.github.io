346d588d9fdb95478b7ad5f4591d14f4f2b25dfc
Sjors Smits <smits.sjors@gmail.com>
2024-10-19 02:41:25 +0200

    Add inter font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./_layouts/**/*.liquid", "./*.{html,md}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "sans": ['"Inter"', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
